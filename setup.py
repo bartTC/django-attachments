d8838c41c44914cb17ed99145bda20b5723d3ae7
Martin Mahner <martin@mahner.org>
2009-05-08 12:27:51 +0200

    Added a long description to the package info. Bumped up to v0.2.1.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-attachments',
-    version='0.2',
+    version='0.2.1',
     description='A generic Django application to attach Files (Attachments) to any model',
+    long_description=open('README.rst').read(),
     author='Martin Mahner',
     author_email='martin@mahner.org',
     url='http://github.com/bartTC/django-attachments/tree/master',
