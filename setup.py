b1f74ca80e62f9dc0fc1d737da4e64e7210e6211
Jannis Leidel <jannis@leidel.info>
2009-05-08 02:02:25 +0200

    Added fraking setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-attachments',
+    version='0.1',
+    description='A generic Django application to attach Files (Attachments) to any model',
+    author='Martin Mahner',
+    author_email='martin@mahner.org',
+    url='http://github.com/bartTC/django-attachments/tree/master',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    package_data = {
+        'attachments': [
+            'templates/attachments/*.html',
+        ]
+    },
+    zip_safe=False,
+)
