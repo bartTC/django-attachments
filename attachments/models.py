0faaae306fabdefc092c47882810fe3a73dcaae0
Martin Mahner <martin@mahner.org>
2009-06-05 15:29:50 +0200

    Removed the dependence of django-extensins. This is backwards compatible.

--- attachments/models.py
+++ attachments/models.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import os
 from django.db import models
 from django.contrib.auth.models import User
-from django_extensions.db.models import TimeStampedModel
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.utils.translation import ugettext_lazy as _
@@ -13,7 +12,7 @@ class AttachmentManager(models.Manager):
         return self.filter(content_type__pk=object_type.id,
                            object_id=obj.id)
 
-class Attachment(TimeStampedModel):
+class Attachment(models.Model):
     def attachment_upload(instance, filename):
         """Stores the attachment in a "per module/appname/primary key" folder"""
         return 'attachments/%s/%s/%s' % (
@@ -29,7 +28,9 @@ class Attachment(TimeStampedModel):
     content_object = generic.GenericForeignKey('content_type', 'object_id')
     creator = models.ForeignKey(User, related_name="created_attachments", verbose_name=_('creator'))
     attachment_file = models.FileField(_('attachment'), upload_to=attachment_upload)
-
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+    
     class Meta:
         ordering = ['-created']
         permissions = (
