c4bf0c0530b37730bf7f00a48f478e9da3eb3c6d
Martin Mahner <martin@mahner.org>
2017-03-18 13:00:27 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = u'\n\n'.join((
 
 setup(
     name='django-attachments',
-    version='1.1',
+    version='1.1.1',
     description='django-attachments is generic Django application to attach '
         'Files (Attachments) to any model.',
     long_description=long_description,
