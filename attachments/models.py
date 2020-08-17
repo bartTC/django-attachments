dc14b907d2b0fb103228609b3a6280828f3bf841
Mr. Senko <atodorov@mrsenko.com>
2020-08-17 16:30:41 +0300

    Add db_index to object_id field
    This field is used in AttachmentManager.attachments_for_object(),
    get_attachments_for template tag!

--- attachments/models.py
+++ attachments/models.py
@@ -31,7 +31,7 @@ class Attachment(models.Model):
     objects = AttachmentManager()
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.TextField()
+    object_id = models.TextField(db_index=True)
     content_object = GenericForeignKey("content_type", "object_id")
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
