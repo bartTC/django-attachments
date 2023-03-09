d7d5dfc2d6196d07002766a42618ed09a4be4c32
Alexander Todorov <atodorov@otb.bg>
2023-03-09 22:42:23 +0200

    Add a model with UUID primary key to be used in tests. Refs #94

--- attachments/tests/testapp/models.py
+++ attachments/tests/testapp/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -6,6 +7,17 @@ class TestModel(models.Model):
 
     class Meta:
         db_table = "testapp_testmodel"
+
+    def get_absolute_url(self):
+        return "/"
+
+
+class ModelWithUuidPk(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "testapp_uuid4_model"
 
     def get_absolute_url(self):
         return "/"
